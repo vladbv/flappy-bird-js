be232ba4c662bca61333efc50bd509da67e9df8a
vladbv <vladimirbhelp@gmail.com>
2020-10-27 02:24:38 +0200

    Added doodle

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
-let datetime = new Date();
+let datetime2 = new Date();
 
-console.log(datetime);
+console.log(datetime2);
 
 const grid = document.querySelector('.grid');
 
