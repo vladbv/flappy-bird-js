7c929e080bce1c6b3242bdf3ee729a359be612af
vladbv <vladimirbhelp@gmail.com>
2020-10-30 03:23:49 +0200

    new changes

--- app.js
+++ app.js
@@ -1,2 +1,41 @@
-console.log('tes');
-console.log('yest');
+
+document.addEventListener('DOMContentLoaded', () => {
+
+const bird = document.querySelector('.bird');
+const gameDisplay = document.querySelector('.game-container');
+const ground = document.querySelector('.ground');
+
+let birdLeft = 220;
+let birdBottom = 100;
+let gravity = 2;
+
+
+
+function startGame(){
+if(birdBottom > 0){birdBottom -= gravity;}
+
+
+bird.style.bottom = birdBottom + 'px';
+
+bird.style.left = birdLeft + 'px';
+
+
+}
+
+let timerId = setInterval(startGame, 20);
+
+
+function jump(event){
+
+if(birdBottom<400)birdBottom += 40;
+
+bird.style.bottom = birdBottom + 'px';
+console.log(birdBottom);
+}
+
+
+document.addEventListener('keyup',jump);
+
+
+});
+
