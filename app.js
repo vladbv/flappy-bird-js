720e05435444e050b1e6520bcd783aebd193d9d2
vladbv <vladimirbhelp@gmail.com>
2020-10-31 02:47:10 +0200

    Newest changes

--- app.js
+++ app.js
@@ -25,17 +25,64 @@ bird.style.left = birdLeft + 'px';
 let timerId = setInterval(startGame, 20);
 
 
-function jump(event){
+function control(e) {
 
-if(birdBottom<400)birdBottom += 40;
+if(e.keyCode === 38){
+
+jump();
+
+}
+
+}
+
+function jump(){
+
+if(birdBottom < 500) birdBottom += 40;
 
 bird.style.bottom = birdBottom + 'px';
 console.log(birdBottom);
 }
 
 
-document.addEventListener('keyup',jump);
+document.addEventListener('keyup', control);
 
+
+
+
+function generateObstacle(){
+
+let obstacleLeft = 500;
+let obstacleBottom = Math.random() * 65;
+
+const obstacle = document.createElement('div');
+
+obstacle.classList.add('obstacle');
+gameDisplay.appendChild(obstacle);
+obstacle.style.left = obstacleLeft + 'px';
+obstacle.style.bottom = obstacleBottom + 'px';
+
+
+
+function moveObstacle(){
+
+obstacleLeft -=5;
+
+obstacle.style.left = obstacleLeft + 'px';
+
+if(obstacleLeft  === 0){
+clearInterval(timed);
+gameDisplay.removeChild(obstacle);
+}
+}
+let timed = setInterval(moveObstacle, 20);
+
+
+setTimeout(generateObstacle, 3000);
+
+}
+
+
+generateObstacle();
 
 });
 
